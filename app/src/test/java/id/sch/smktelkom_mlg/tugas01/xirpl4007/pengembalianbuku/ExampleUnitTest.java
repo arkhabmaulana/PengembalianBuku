ee9161daa68cf768128318c764fcd7d28c692f43
Arkhab Maulana <alf1pembalap@gmail.com>
2016-09-09 09:10:28 +0700

    First Commit : Judul Project

---
+++ app/src/test/java/id/sch/smktelkom_mlg/tugas01/xirpl4007/pengembalianbuku/ExampleUnitTest.java
@@ -0,0 +1,15 @@
+package id.sch.smktelkom_mlg.tugas01.xirpl4007.pengembalianbuku;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * To work on unit tests, switch the Test Artifact in the Build Variants view.
+ */
+public class ExampleUnitTest {
+    @Test
+    public void addition_isCorrect() throws Exception {
+        assertEquals(4, 2 + 2);
+    }
+}
